5006229c7cddd79c3a4b9a700d8f87cb7e8c1401
AdmiralSpeedy <admrlspeedy@hotmail.com>
2015-11-22 15:10:16 -0600

    Complete rewrite, allowing the arm and claw to be fully controlled.

--- Servos/Servos.ino
+++ Servos/Servos.ino
@@ -1,71 +1,162 @@
 /*
-	Multiple Servo Control
+	Multiple Servo Arm Control
 	
-	Requires two servos assembled together with a Lynxmontion Pan/Tilt Kit.
-
-	Wire the base servo to pin 9 and the top servo to pin 10.
+	Requires five servos. Two for pan and tilt (base and shoulder), one for the elbow and two for the claw.
+		- Base servo on pin 9
+		- Shoulder servo on pin 10
+		- Elbow servo on pin 11
+		- Claw right servo on pin 12
+		- Claw left servo on pin 13
 	
-	Servos are currently controlled via serial as follows (until Bluetooth is implemented):
+	Servo characters:
+		B(66) = Base
+		S(83) = Shoulder
+		E(69) = Elbow
+		C(67) = Claw
 	
-		Values are entered as "0,0" (Servo 1 position, Servo 2 position)
-		
-		Servo 1 range: 0 degrees - 180 degrees
-		Servo 2 range: 0 degrees - 135 degrees (to avoid collision with bracket)
+	To control each servo you simply send the servo character for the servo followed by a value.
+	Example: "E180" will set the elbow servo to 180 degrees.
+	Values that are out of range for the specified servo are checked and adjusted accordingly.
 
 	Created by Mitchell Steffensen
 */
 
 #include <Servo.h> 
- 
-Servo servo1; //Base servo object
-Servo servo2; //Top servo object
 
-long servo1Pos = 0; //Default starting position for the base servo
-long servo2Pos = 90; //Default starting position for the top servo
+//Servo pins
+const int BASE_SERVO_PIN = 9;
+const int SHOULDER_SERVO_PIN = 10;
+const int ELBOW_SERVO_PIN = 11;
+const int CLAW_RIGHT_SERVO_PIN = 12;
+const int CLAW_LEFTT_SERVO_PIN = 13;
+
+//Servo ranges
+const int BASE_SERVO_MAX = 180;
+const int BASE_SERVO_MIN = 0;
+const int SHOULDER_SERVO_MAX = 135;
+const int SHOULDER_SERVO_MIN = 0;
+const int ELBOW_SERVO_MAX = 180;
+const int ELBOW_SERVO_MIN = 30;
+const int CLAW_RIGHT_SERVO_MAX = 180;
+const int CLAW_RIGHT_SERVO_MIN = 75;
+const int CLAW_LEFT_SERVO_MAX = 105;
+const int CLAW_LEFT_SERVO_MIN = 0;
+
+const int SERVO_SPEED = 20; //Servo speed (Delay between each servo movement)
  
+//Servo objects
+Servo baseServo;
+Servo shoulderServo;
+Servo elbowServo;
+Servo clawRightServo;
+Servo clawLeftServo;
+
+//Current servo positions
+long baseServoPos = 0;
+long shoulderServoPos = 90;
+long elbowServoPos = 120;
+long clawRightServoPos = 75;
+long clawLeftServoPos = 105;
+
+//Last known servo positons
+long baseServoLastPos = baseServoPos;
+long shoulderServoLastPos = shoulderServoPos;
+long elbowServoLastPos = elbowServoPos;
+
+int servoByte = 0; //First byte of serial data to check which servo(s) to control
+
 void setup() 
 { 
 	Serial.begin(9600); //Open the serial port
 	
 	//Attach the servo's to their objects
-	servo1.attach(9);
-	servo2.attach(10);
+	baseServo.attach(BASE_SERVO_PIN);
+	shoulderServo.attach(SHOULDER_SERVO_PIN);
+	elbowServo.attach(ELBOW_SERVO_PIN);
+	clawRightServo.attach(CLAW_RIGHT_SERVO_PIN);
+	clawLeftServo.attach(CLAW_LEFTT_SERVO_PIN);
 	
 	//Move the servos to their default positions
-  	servo1.write(servo1Pos);
-	servo2.write(servo2Pos);
+  	baseServo.write(baseServoPos);
+	shoulderServo.write(shoulderServoPos);
+	elbowServo.write(elbowServoPos);
+	clawLeftServo.write(clawLeftServoPos);
+	clawRightServo.write(clawRightServoPos);
 } 
  
 void loop() 
 { 	
 	if(Serial.available() > 0) //There is available serial data
 	{
-		//Read input for the servo positions
-		servo1Pos = Serial.parseInt();
-		servo2Pos = Serial.parseInt();
+		servoByte = Serial.read(); //Read the first byte of the serial data to determine which servo(s) to control
 		
-		//The input is out of the possible range for the servo so adjust it accordingly
-		if(servo1Pos < 0)
+		if(servoByte == 66) //Base
 		{
-			servo1Pos = 0;
+			moveServo(baseServo, baseServoPos, baseServoLastPos, BASE_SERVO_MAX, BASE_SERVO_MIN, SERVO_SPEED); 
 		}
-		else if(servo1Pos > 180)
+		else if(servoByte == 83) //Shoulder
 		{
-			servo1Pos = 180;
+			moveServo(shoulderServo, shoulderServoPos, shoulderServoLastPos, SHOULDER_SERVO_MAX, SHOULDER_SERVO_MIN, SERVO_SPEED);
 		}
-		
-		//The input is out of the possible range for the servo so adjust it accordingly
-		if(servo2Pos < 0)
+		else if(servoByte == 69) //Elbow
 		{
-			servo2Pos = 0;
+			moveServo(elbowServo, elbowServoPos, elbowServoLastPos, ELBOW_SERVO_MAX, ELBOW_SERVO_MIN, SERVO_SPEED);
 		}
-		else if(servo2Pos > 135)
+		else if(servoByte == 67) //Claw
 		{
-			servo2Pos = 135;
+			clawRightServoPos = Serial.parseInt(); //Read input for the servo position
+			
+			//The input is out of the possible range for the servo so adjust it accordingly
+			if(clawRightServoPos < CLAW_RIGHT_SERVO_MIN)
+			{
+				clawRightServoPos = CLAW_RIGHT_SERVO_MIN;
+			}
+			else if(clawRightServoPos > CLAW_RIGHT_SERVO_MAX)
+			{
+				clawRightServoPos = CLAW_RIGHT_SERVO_MAX;
+			}
+	
+			clawLeftServoPos = 180 - clawRightServoPos; //Invert the value for the left claw
+			
+			//Move both sides of the claw
+			clawRightServo.write(clawRightServoPos);
+			clawLeftServo.write(clawLeftServoPos);
 		}
-		
-		//Change the position of each servo
-		servo1.write(servo1Pos);
-		servo2.write(servo2Pos);
 	}
-} 
+}
+
+void moveServo(Servo servo, long newPos, long &lastPos, int max, int min, int moveDelay)
+{
+	newPos = Serial.parseInt(); //Read input for the servo position
+	
+	//The input is out of the possible range for the servo so adjust it accordingly
+	if(newPos < min)
+	{
+		newPos = min;
+	}
+	else if(newPos > max)
+	{
+		newPos = max;
+	}
+	
+	if(newPos > lastPos) //The new position is greater than the last position
+	{
+		//Loop up from the old position to the new position, moving the servo 1 degree at a time with a delay in between each movement
+		for (int i = lastPos; i <= newPos; i++)
+		{
+			servo.write(i);
+			delay(moveDelay);
+		}
+	}
+	else //The new position is less than the last position
+	{
+		//Loop down from the old position to the new position, moving the servo 1 degree at a time with a delay in between each movement
+		for (int i = lastPos; i >= newPos; i--)
+		{
+			servo.write(i);
+			delay(moveDelay);
+		}
+	}
+	
+	lastPos = newPos; //Update the last known position
+}
